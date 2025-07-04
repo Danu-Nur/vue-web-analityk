f206371c7afbe302b07556e4ea64ed091eb0ac88
Danu-Nur <diramadhan13@gmail.com>
2025-07-04 15:59:45 +0700

    feat: update article layout in Behavior.vue for Click and Custom Events sections to span full width

--- src/pages/admin/Behavior.vue
+++ src/pages/admin/Behavior.vue
@@ -41,7 +41,7 @@ const scale = computed(() => {
         <section aria-label="Main content" class="grid grid-cols-1 lg:grid-cols-2 gap-4 sm:gap-6 mb-6">
             <!-- content 1 -->
             <div class="grid grid-cols-1 md:grid-cols-2 gap-6 order-2 lg:order-none h-fit">
-                <article aria-label="Click Events" class="border border-gray-200 rounded-lg p-4 flex flex-col">
+                <article aria-label="Click Events" class="border md:col-span-2 border-gray-200 rounded-lg p-4 flex flex-col">
                     <h2 class="font-semibold text-gray-900 mb-4">Click Events</h2>
                     <Suspense>
                         <template #default>
@@ -80,7 +80,7 @@ const scale = computed(() => {
                         </template>
                     </Suspense>
                 </article>
-                <article aria-label="Custom Events" class="border border-gray-200 rounded-lg p-4 flex flex-col">
+                <article aria-label="Custom Events" class="border md:col-span-2 border-gray-200 rounded-lg p-4 flex flex-col">
                     <h2 class="font-semibold text-gray-900 mb-4">Custom Events</h2>
                     <Suspense>
                         <template #default>
